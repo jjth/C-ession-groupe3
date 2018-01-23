06cfb669fcb2c82fa7965cb22c3d175128a1d07a
DESKTOP-539JE3P\PC <viincerome@gmail.com>
2018-01-23 11:43:46 +0100

    Ajout de la portabilité Linux ainsi que test à réaliser sur l'envoie d'une chaine de caractère

--- src/modules/ClientLinux.c
+++ src/modules/ClientLinux.c
@@ -1,12 +1,20 @@
-#if defined(WIN32) || defined(WIN32) || (defined(CYGWIN_) && !defined(_WIN32))
-    #define WIN32 0
-    #include <winsock2.h> 
-    typedef int socklen_t;
+#if defined (linux)
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define closesocket(s) close(s)
+    typedef int SOCKET;
+    typedef struct sockaddr_in SOCKADDR_IN;
+    typedef struct sockaddr SOCKADDR;
 #endif
  
 #include <stdio.h>
 #include <stdlib.h>
-#define PORT 23
+#define PORT 766
  
  
  
--- src/modules/server.c
+++ src/modules/server.c
@@ -1,14 +1,15 @@
-#if defined (WIN32)
-    #include <winsock2.h>
+#if defined(WIN32) || defined(WIN32) || (defined(CYGWIN_) && !defined(_WIN32))
+    #define WIN32 0
+    #include <winsock2.h> 
     typedef int socklen_t;
 #endif
- 
+
 #include <stdio.h>
 #include <stdlib.h>
-#define PORT 23
+#define PORT 766
  
  
- 
+
 int main(void)
 {
     #if defined (WIN32)
