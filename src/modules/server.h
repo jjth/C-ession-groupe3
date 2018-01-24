3faea81defa222923f5a9bb8a1a08ad2cf531aa2
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2018-01-24 16:34:01 +0100

    server v1 ok

--- src/modules/server.h
+++ src/modules/server.h
@@ -25,7 +25,6 @@ typedef struct in_addr IN_ADDR;
 
 #define CRLF		"\r\n"
 #define PORT	 	766
-#define MAX_CLIENTS 	2
 
 #define BUF_SIZE	1024
 
@@ -39,11 +38,7 @@ static void init(void);
 static void end(void);
 static void app(void);
 static int init_connection(void);
-static void end_connection(int sock);
 static int read_client(SOCKET sock, char *buffer);
 static void write_client(SOCKET sock, const char *buffer);
-static void send_message_to_all_clients(Client *clients, Client client, int actual, const char *buffer, char from_server);
-static void remove_client(Client *clients, int to_remove, int *actual);
-static void clear_clients(Client *clients, int actual);
 
 #endif /* guard */
