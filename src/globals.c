1660b159e7407cbbcb94b21811f839c366be37fc
Jean Thévenet <jjth01@gmail.com>
2018-01-25 00:33:12 +0100

    [Project] Add str_split() split a string into a array of strings

--- src/globals.c
+++ src/globals.c
@@ -3,6 +3,19 @@
 #include <string.h>
 #include "globals.h"
 
+#if defined(WIN32) || defined(_WIN32) || (defined(CYGWIN_) && !defined(_WIN32))
+char *strsep(char **stringp, const char *delim) {
+  if (*stringp == NULL) { return NULL; }
+  char *token_start = *stringp;
+  *stringp = strpbrk(token_start, delim);
+  if (*stringp) {
+    **stringp = '\0';
+    (*stringp)++;
+  }
+  return token_start;
+}
+#endif
+
 CommandType get_command(char* string) {
     if (strlen(string) < 10) {
         return CMD_UNKNOWN;
@@ -43,4 +56,30 @@ char* get_recv_data(char* string) {
     data[new_str_size] = '\0';
 
     return data;
+}
+
+char** str_split(char* string) {
+    char* token;
+    char* str = strdup(string);
+    char* toFree;
+    char** result;
+    int i;
+
+    toFree = str = strdup(string);
+    // Count the number of ";"
+    for (i = 0; str[i]; str[i] == ';' ? i++ : *str++);
+    free(toFree);
+
+    result = malloc(sizeof(char*)*(i+1));
+
+    toFree = str = strdup(string);
+    i = 0;
+    while((token = strsep(&str, ";"))) {
+        result[i] = token;
+        i++;
+    }
+
+    free(str);
+
+    return result;
 }
